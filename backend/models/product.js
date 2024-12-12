cfcd80219de9335df32378c258c9fd3774cde461
LilDavid <dat.dotuan@hcmut.edu.vn>
2024-12-12 16:25:36 +0700

    Thêm chi tiết các lỗi khi thêm và cập nhật sản phẩm. sửa lại thủ tục thêm sản phẩm.

--- backend/models/product.js
+++ backend/models/product.js
@@ -5,12 +5,18 @@ const themSanPham = (p_MoTa, p_Loai, p_TenSanPham, p_MaDanhMuc, p_MaCuaHang, p_S
     const sql = 'CALL ThemSanPham(?, ?, ?, ?, ?, ?, ?)';
     db.query(sql, [p_MoTa, p_Loai, p_TenSanPham, p_MaDanhMuc, p_MaCuaHang, p_SoLuong, p_Gia], (err, result) => {
       if (err) {
-        reject(err); 
+        reject(err);
       }
       resolve(result); 
     });
   });
 };
+
+module.exports = {
+  themSanPham
+};
+
+
 
 const getAllProducts = () => {
   return new Promise((resolve, reject) => {
@@ -28,3 +34,14 @@ module.exports = {
   themSanPham,
   getAllProducts
 };
+// const themSanPham = (p_MoTa, p_Loai, p_TenSanPham, p_MaDanhMuc, p_MaCuaHang, p_SoLuong, p_Gia) => {
+//   return new Promise((resolve, reject) => {
+//     const sql = 'CALL ThemSanPham(?, ?, ?, ?, ?, ?, ?)';
+//     db.query(sql, [p_MoTa, p_Loai, p_TenSanPham, p_MaDanhMuc, p_MaCuaHang, p_SoLuong, p_Gia], (err, result) => {
+//       if (err) {
+//         reject(err); 
+//       }
+//       resolve(result); 
+//     });
+//   });
+// };
